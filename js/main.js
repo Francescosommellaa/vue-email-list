754a2a544809110fa391ebbaef738dd88282f109
Francescosommellaa <franc.sommella@gmail.com>
2024-06-28 16:45:35 +0200

    add:  for loop for email generation

--- js/main.js
+++ js/main.js
@@ -5,26 +5,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            // Inizializza una variabile per gli indirizzi email
-            mailList: "",
-        }
-    },
-    methods: {
-
+            mailList: []
+        };
     },
     mounted() {
-        // Effettua una richiesta GET all'API per ottenere 10 indirizzi email casuali
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((risposta) => {
-                // Estrai gli indirizzi email dalla risposta
-                const emails = risposta.data.response;
-                console.log(emails);
-                // Assegna gli indirizzi email alla variabile mailList
-                this.mailList = emails;
-            })
-            .catch(function (error) {
-                // Gestisci eventuali errori
-                console.log(error.message);
-            });
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const emails = response.data.response;
+                    console.log(emails);
+                    this.mailList.push(emails);
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+        }
     }
 }).mount('#app'); // Monta l'app Vue nell'elemento con id "app"
