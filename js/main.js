360b6d96c0ad4f72bb29bab37ab09f6773a171b1
Francescosommellaa <franc.sommella@gmail.com>
2024-06-28 16:48:42 +0200

    ADD: comment js

--- js/main.js
+++ js/main.js
@@ -5,17 +5,23 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            // crea array per le email
             mailList: []
         };
     },
     mounted() {
+        // inizializzo il ciclo for e gli dico di fermarsi quando arrriva a 10 email
         for (let i = 0; i < 10; i++) {
+            // richiamo axios per poter recuperare l'email randomica
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
+                    // recupero l'email
                     const emails = response.data.response;
                     console.log(emails);
+                    // inserisco l'email nell'array
                     this.mailList.push(emails);
                 })
+                // gestisco eventuali errori
                 .catch((error) => {
                     console.log(error.message);
                 });
